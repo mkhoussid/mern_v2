06e21858ff6116cadf82f5597d86850a5795c571
Michael Khoussid <khmike@hotmail.ca>
2019-05-29 06:57:46 -0400

    fixes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -159,4 +159,21 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route  DELETE api/profile
+// @desc   Delete profile
+// @access Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
