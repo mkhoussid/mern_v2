e5e836c0fc1dde5b65dd08d8daaf274b982f7a46
Michael Khoussid <khmike@hotmail.ca>
2019-05-30 15:52:08 -0400

    added create profile button

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,15 +1,42 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
+
+import { Link } from "react-router-dom";
 
 import { connect } from "react-redux";
 
 import { getCurrentProfile } from "../../actions/profile";
 
-const Dashboard = ({ profile, auth, getCurrentProfile }) => {
+import Spinner from "../layout/Spinner";
+
+const Dashboard = ({
+  profile: { profile, loading },
+  auth: { user },
+  getCurrentProfile
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
-  return <div>Dashboard</div>;
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <Fragment>has</Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not yet set up a profile! Please add some info</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 const mapStateToProps = state => ({
