e6e34f82f20aa4faf51bcea30037528ec124d241
Michael Khoussid <khmike@hotmail.ca>
2019-05-30 12:18:22 -0400

    fixes

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,7 +1,36 @@
 import axios from "axios";
 import { setAlert } from "./alert";
 
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
+} from "./types";
+
+import setAuthToken from "../utils/setAuthToken";
+
+// Load user
+export const loadUser = () => async dispatch => {
+  const token = localStorage.token;
+
+  if (token) {
+    setAuthToken(token);
+  }
+
+  try {
+    const response = await axios.get("/api/auth");
+
+    dispatch({
+      type: USER_LOADED,
+      payload: response.data
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR
+    });
+  }
+};
 
 // Register user
 export const register = ({ name, email, password }) => async dispatch => {
